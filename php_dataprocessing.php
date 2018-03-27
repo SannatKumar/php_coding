8574abbf046278db1151ce583c52b43787e58928
Raj <rz_kumr@yahoo.com>
2018-03-27 23:17:44 +0300

    This is a form which collects data for the php_dataprocessing.php

---
+++ php_dataprocessing.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<html>
+<body>
+<form action = "php_dataprocessing_echo.php" method = "post">
+Name: <input type = "text" name = "name"><br><br>
+E-mail id: <input type = "text" name = "email"><br><br>
+<input type = "Submit">
+</form>
+</body>
+</html>
