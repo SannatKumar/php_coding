006a14b2d148b211d83844d5643524e1a841556e
Raj <rz_kumr@yahoo.com>
2018-03-27 23:15:50 +0300

    This prints the name and email from php_dataprocessing.php file

---
+++ php_dataprocessing_echo.php
@@ -0,0 +1,11 @@
+<?php
+// This is a php file that php_dataprocessing.php action into
+?>
+<html>
+<body>
+Welcome Mr/Ms <?php echo $_POST["name"]; ?> <br> <!--Here we print the name from post method-->
+Your email id is: <?php echo $_POST["email"] ; ?>
+Thank you
+:)
+</body>
+</html>
